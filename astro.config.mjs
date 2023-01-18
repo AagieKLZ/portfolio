0fb4566bbeddc184d950f9cd1522ce99118dc62f
AagieKLZ <darkyelan@gmail.com>
2023-01-18 19:40:06 +0100

    Added setup for Vercel deployment and fixed responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/edge";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   config: {
     applyBaseStyles: true
-  },
-  output: "server",
-  adapter: vercel()
+  }
 });
